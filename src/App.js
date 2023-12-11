83a3cbbcfe2da3e2a785df8094b21fde67543c1d
Carlos Javier Aceves Perez <ciap1382@gmail.com>
2023-12-11 14:54:43 -0600

    incluimos encabezado e instalamos Bootstrap por problemas al centrar elementos con css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import RecipeReviewCard from "./Site/2-Contenidos/CardMaterialUI"
+import RecipeReviewCard from "./Site/2-Contenidos/CardMaterialUI";
+import AccountMenu from "./Site/1-Encabezados/MenuMaterialUI";
+import "./Assets/css/index.css"
 const App =() => {
   return (
-    <div className="App">
-      <RecipeReviewCard/>
+    <>
+    <div className="menu">
+      <AccountMenu/>
     </div>
+    <div className="gallery">
+      <div className="cardContent"><RecipeReviewCard/></div>
+      <div className="cardContent"><RecipeReviewCard/></div>
+      <div className="cardContent"><RecipeReviewCard/></div>      
+    </div>
+    </>
   );
 };
 export default App;
